d9b302c2ae3f759e6e8b9a2aaa7171e798f7ee6c
Natalia Potracka <n.potracka@gmail.com>
2023-06-05 16:17:10 +0200

    add script.js

--- script.js
+++ script.js
@@ -16,5 +16,19 @@ printMessage('Zagrałem ' + playerMove + '');
 
 
 var randomNumber;
-randomNumber = Math.floor(Math.random() * 19 + 1);
+randomNumber = Math.floor(Math.random() * (19-11) + 11);
 printMessage('Wylosowana liczba to: ' + randomNumber);
+// W poleceniu było liczby od 11 do 19. Tu mam problem, bo nie wiem jak to napisać. Logicznie należałoby zrobić jakiś przedział, ale jak to napisać w JavaScript 🤷‍♀️.
+// Poprawione, musiałam podpytać Internety.
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
